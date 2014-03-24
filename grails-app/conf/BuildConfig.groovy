74f45d1faad55970ee58d50d8a7fdcbee4180a69
Soeren Berg Glasius <soeren@glasius.dk>
2014-03-24 11:44:19 +0100

    Updated to latest release plugins and fixed to allow for maven-install

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
 
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
 
     inherits("global") {
@@ -19,10 +20,11 @@ grails.project.dependency.resolution = {
     }
 
 	plugins {
-        build(":release:3.0.1") {
+        build(":release:3.0.1",
+                ":rest-client-builder:2.0.1") {
             export = false
         }
-        runtime ":tomcat:7.0.42"
+        runtime ":tomcat:7.0.52.1"
     }
 
 }
